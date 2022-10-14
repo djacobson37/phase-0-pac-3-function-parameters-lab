69ab2356934cf6014937ebb96b14d9d780a10ac2
David F. Jacobson <jakelis2018@gmail.com>
2022-10-14 10:03:54 -0500

    commit tree e9437cc64261a586244aa30c79857315fe1d091b

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
+}
+introduction("Aki");
+introduction("Samip");
+
+function introductionWithLanguage(name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+introduction("Aki", "Ember.js");
+introduction("Samip", "React");
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+introductionWithLanguageOptional("Gracie");
